035f8967e62605eccfbbb737beb90105db55fe64
Vitaly Likhachev <vitaly99878@gmail.com>
2024-06-05 14:34:24 +0300

    pm-1 Инициализация проекта
    Добавил Prettier
    Нужно добавить еще eslint, commit msg hook, readme описание

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import express from "express";
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req: any, res: any) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log('Hello World')
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log('Hello World');
+    console.log(`Example app listening on port ${port}`);
+});
